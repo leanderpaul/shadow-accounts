4a126678f53b533eddf05e37714d74dfc1d787c2
Leander Paul <leanderpaul97@outlook.com>
2023-11-12 23:09:15 +0000

    feat: config service

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import handlebars from 'handlebars';
  * Importing user defined packages
  */
 import { AppModule } from './app.module';
+import { Config } from './services';
 
 /**
  * Defining types
@@ -22,14 +23,14 @@ import { AppModule } from './app.module';
 /**
  * Declaring the constants
  */
-const publicDir = path.join(__dirname, '..', 'public');
-const templateDir = path.join(__dirname, '..', 'views');
 
 async function bootstrap() {
+  const publicDir = path.join(__dirname, '..', 'public');
+  const templateDir = path.join(__dirname, '..', 'views');
+
+  /** Creating fasitfy instance and registering plugins */
   const adapter = new FastifyAdapter();
   const instance = adapter.getInstance();
-
-  /** Registering fastify plugins */
   await instance.register(fastifyCookie);
   await instance.register(compression);
 
@@ -39,7 +40,10 @@ async function bootstrap() {
   app.setViewEngine({ engine: { handlebars }, templates: templateDir });
   app.enableShutdownHooks([ShutdownSignal.SIGINT, ShutdownSignal.SIGUSR2, ShutdownSignal.SIGTERM]);
 
-  await app.listen(8080);
+  /** Starting the nestjs application */
+  const port = Config.get('app.port');
+  const hostname = Config.get('app.hostname');
+  await app.listen(port, hostname);
 }
 
 bootstrap();
---
+++ src/services/config.service.ts
@@ -0,0 +1,51 @@
+/**
+ * Importing npm packages
+ */
+import { type ConfigRecords, ConfigService } from '@leanderpaul/shadow-service';
+
+/**
+ * Importing user defined packages
+ */
+
+/**
+ * Defining types
+ */
+
+export interface ServerConfigRecords extends ConfigRecords {
+  /** Server configs */
+  'app.hostname': string;
+  'app.port': number;
+  'app.domain': string;
+
+  /** Database configs */
+  'db.uri': string;
+
+  /** Authentication configs */
+  'csrf.secret-key': Buffer;
+  'refresh-token.secret-key': Buffer;
+}
+
+/**
+ * Declaring the constants
+ */
+
+class ServerConfigService extends ConfigService<ServerConfigRecords> {
+  constructor() {
+    super('shadow-accounts');
+
+    this.set('app.hostname', { defaultValue: '0.0.0.0' });
+    this.set('app.port', { defaultValue: '8080', validateType: 'number' });
+    this.set('app.domain', { defaultValue: 'localhost' });
+
+    this.set('db.uri', { defaultValue: 'mongodb://localhost/shadow-accounts' });
+
+    const arr = new Array(43);
+    const transformSecretKey = (value: string): Buffer => Buffer.from(value, 'base64');
+    const secretKeyValidator = (value: string): boolean => Buffer.from(value, 'base64').length === 32;
+    this.set('csrf.secret-key', { defaultValue: arr.fill('A').join(''), validator: secretKeyValidator, transform: transformSecretKey });
+    this.set('refresh-token.secret-key', { defaultValue: arr.fill('B').join(''), validator: secretKeyValidator, transform: transformSecretKey });
+  }
+}
+
+const globalRef = global as any;
+export const Config: ServerConfigService = globalRef.configService || (globalRef.configService = new ServerConfigService());
