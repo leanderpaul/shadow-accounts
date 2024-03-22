c401225842e2a4ab29e5425701c1651bbeaaa6d7
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-03-22 22:33:12 +0400

    fix: test script and test workflow (#59)

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,26 +1,16 @@
-/**
- * Importing npm packages
- */
+/**Importing npm packages */
 import { spawn } from 'node:child_process';
 import colors from '@colors/colors/safe';
 
-/**
- * Importing user defined packages
- */
-
-/**
- * Defining types
- */
-
-/**
- * Declaring the constants
- */
+/** Declaring the constants */
 const { gray, green, yellow } = colors;
 const env = { ...structuredClone(process.env), NODE_ENV: 'development' };
 
+/** Starting the server and tailwindcss */
 const server = spawn('bun', ['run', '--watch', 'src/main.ts'], { stdio: 'inherit', env });
 const tailwindcss = spawn('bun', ['run', '-i', 'build:css', '--watch'], { stdio: 'pipe', env });
 
+/** Logging the tailwind css output */
 tailwindcss.stderr.on('data', data => {
   const message = data.toString().trim();
   if (message.startsWith('Done in')) {
---
+++ scripts/test.ts
@@ -0,0 +1,44 @@
+/**Importing npm packages */
+import { type SpawnSyncReturns, spawnSync } from 'node:child_process';
+import fs from 'node:fs';
+import colors from '@colors/colors/safe';
+
+/** Declaring the constants */
+const { red, bold, brightBlue } = colors;
+const printTitle = (title: string) => console.log('\n' + brightBlue(bold(title)) + '\n');
+const env = { ...structuredClone(process.env), NODE_ENV: 'test' };
+const testArg = process.argv[2];
+const runUnitTests = testArg === undefined || testArg === 'unit';
+const runUITests = testArg === undefined || testArg === 'ui';
+const runAPITests = testArg === undefined || testArg === 'api';
+const processResult = (result: SpawnSyncReturns<Buffer>) => result.status != null && result.status !== 0 && process.exit(result.status);
+
+/** Checking if the test argument is valid */
+if (!runUnitTests && !runUITests && !runAPITests) {
+  const error = red("Invalid test argument\nValid test arguments are: 'unit', 'ui', 'api'\nLeave empty to run all tests");
+  console.error(error);
+  process.exit(1);
+}
+
+/** Deleting the previous test results and reports if exists */
+fs.rmSync('playwright-report', { recursive: true, force: true });
+fs.rmSync('test-results', { recursive: true, force: true });
+
+/** Running the tests */
+if (runUnitTests) {
+  printTitle('Running unit tests');
+  const result = spawnSync('bun', ['test', 'modules', '--preload', 'tests/modules/setup.ts'], { stdio: 'inherit', env });
+  processResult(result);
+}
+
+if (runAPITests) {
+  printTitle('Running API tests');
+  const result = spawnSync('bun', ['test', 'apis', '--preload', 'tests/apis/setup.ts'], { stdio: 'inherit', env });
+  processResult(result);
+}
+
+if (runUITests) {
+  printTitle('Running UI tests\n');
+  const result = spawnSync('bunx', ['playwright', 'test'], { stdio: 'inherit' });
+  processResult(result);
+}
